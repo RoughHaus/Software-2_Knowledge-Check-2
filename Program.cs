9acb63b6067cb5cb795ae06b26b9b8eb9175cacb
RoughHaus <152419914+RoughHaus@users.noreply.github.com>
2024-05-16 13:43:00 -0400

    Completed Knowledge Check 2
    Program.cs file for functional & finished result of Knowledge Check 2

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Enter how many jets are present on the radar scope: ");
+var numberOfRecords = int.Parse(Console.ReadLine());
+
+var recordList = new List<Jet>();
+for (int i = 0; i < numberOfRecords; i++)
+{
+    // In this loop, populate the object's properties using Console.ReadLine()
+    var jet = new Jet();
+
+    Console.WriteLine("Enter the number of engines for the jet: ");
+    jet.Engines = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Enter the altitude for the jet: ");
+    jet.Altitude = int.Parse(Console.ReadLine());
+
+    recordList.Add(jet);
+}
+
+// Print out the list of records using Console.WriteLine()
+foreach (var jet in recordList)
+{
+    Console.WriteLine(jet.ToString());
+}
+
+public class Airplane
+{
+    public int Engines;
+    public int Altitude;
+
+    public override string ToString()
+    {
+        return $"Engines : {Engines}, Altitude: {Altitude}";
+    }
+}
+
+public class Jet : Airplane 
+{
+    private int propellers = 0;
+}
